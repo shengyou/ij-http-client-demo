989d11ccdc13950c0305eae47c9b76ad90bc3b26
Shengyou Fan <shengyoufan@gmail.com>
2024-10-15 18:04:25 +0800

    project init

---
+++ src/main/kotlin/com/jetbrains/demo/ijhttpclient/IjHttpClientDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.jetbrains.demo.ijhttpclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IjHttpClientDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<IjHttpClientDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/jetbrains/demo/ijhttpclient/IjHttpClientDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jetbrains.demo.ijhttpclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IjHttpClientDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
